f39e978f6d5bfa8f158bffda176e4cf7f8665530
Tomislav996 <trpeskitomislav@outlook.com>
2023-11-16 17:24:50 +0100

    update

---
+++ hotel-react/src/context/RoomContext.js
@@ -0,0 +1,43 @@
+import React, { createContext, useEffect, useState } from "react";
+
+import { roomData } from "../data";
+
+export const RoomContext = createContext();
+
+const RoomProvider = ({ children }) => {
+  const [rooms, setRooms] = useState(roomData);
+  const [adults, setAdults] = useState("1 Adult");
+  const [kids, setKids] = useState("0 Children");
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTotal(Number(adults[0]) + Number(kids[0]));
+  }, [adults, kids]);
+
+  const handleClick = (event) => {
+    
+    event.preventDefault();
+
+    setLoading(true);
+
+    const newRooms = roomData.filter((room) => {
+      return total <= room.maxPerson;
+
+    });
+    setTimeout(() => {
+      setRooms(newRooms);
+      setLoading(false);
+    }, 3000);
+  };
+
+  return (
+    <RoomContext.Provider
+      value={{ rooms, adults, kids, setAdults, setKids, total, handleClick, loading }}
+    >
+      {children}
+    </RoomContext.Provider>
+  );
+};
+
+export default RoomProvider;
